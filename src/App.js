2c36c00415c7da9c47cd4888d4df9d2fb5d8b7a5
ethankaloi <ekaloi123@gmail.com>
2022-10-27 13:11:37 -0600

    Finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: '', cities:[{"city":"Provo"},{"city":"Lindon"}]};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    var url = "https://csonline.byu.edu/city?q=" + event.target.value;
+    console.log("URL " + url);
+    fetch(url)
+      .then((data) => {
+        return (data.json());
+      })
+      .then((citylist) => {
+        console.log("CityList");
+        console.log(citylist);
+        this.setState({cities:citylist})
+        console.log(this.state.cities);
+      });
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    const listItems = this.state.cities.map((cityname) => 
+          <li key={cityname.city}>{cityname.city}</li>
+        );
+     
+    return (
+        <div>
+        <form onSubmit={this.handleSubmit} onKeyUp={this.handleChange}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+        </form>
+        <ul>{listItems}</ul>
+        </div>
+    );
+  }
 }
+
+
 
 export default App;
